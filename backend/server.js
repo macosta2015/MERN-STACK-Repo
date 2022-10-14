de010889e1e802775066bfa9b313936faae20c34
macosta2015 <macosta2015@my.fit.edu>
2022-10-14 15:25:13 -0400

    Finished 4th tutorial

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,31 @@
 require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
+  console.log(req.path, req.method)
+  next()
 })
 
 // routes
-app.get('/', (req,res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, ()=>{
-    console.log('listening on port 4000')
+// connect to db
+mongoose.connect(process.env.MONG_URI)
+.then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log('listening on port', process.env.PORT)
+    })
 })
-
+.catch((error) =>{
+    console.log(error)
+})
