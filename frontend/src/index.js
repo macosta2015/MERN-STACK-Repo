df954965a63621864aff0f3c2102777cba6946ca
macosta2015 <macosta2015@my.fit.edu>
2022-10-25 23:46:48 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,7 @@
+//Practice makes perfect. If we code everyday, it will become a habit. NEED TO DO IT EVEN WHEN I DON'T FEEL IT AND REWARD MYSELF (LOOK FOR FEEDBACK)
+
+// LOVE YOU, MARIO
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
