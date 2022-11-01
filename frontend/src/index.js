c8624267ca573dc2afdb04cd4328de00fa474e9d
macosta2015 <macosta2015@my.fit.edu>
2022-11-01 00:31:39 -0400

    lesson 8

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,13 +7,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from './context/WorkoutContext';
+import { WorkoutsContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
-    <App />
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+      <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
